f0fe39982f464790ed505f3929409f560e7d0df2
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-03-15 15:56:20 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def breed=("Mutt")
+    @breed = "#{breed}"
+  end
+  def breed
+    @breed
+  end
+end
